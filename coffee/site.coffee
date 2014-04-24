df45fd4390e6e517fa39fb51f3b4f68918830dd9
armankara <armankara19@gmail.com>
2014-04-24 22:51:23 +0300

    first commit

---
+++ coffee/site.coffee
@@ -0,0 +1,4 @@
+document.getElementById('button').onclick = ->
+	alert('tıkladım')
+
+	
---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //pluginlerin kendi konfigürasyon parametrelerini
+        coffee: {
+            compile: {
+                files: {
+                    'site.js': 'coffee/site.coffee' // site.coffee'de yazdıklarımızı site.js' compile ediyoruz
+
+                }
+
+            }
+
+        },
+        less: { //pluginlerin kendi konfigürasyon parametreleri
+            compile: {
+                files: {
+                    'site.css': 'less/site.less' // site.less'de yazdıklarımızı site.css' compile ediyoruz
+
+                }
+
+            }
+
+        },
+
+
+
+    });
+    // Yüklenecek grunt plugin'leri
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    //Burada da task'leri oluşturuyoruz
+    //Burada default task gidip  "coffee" ve "less" taskini çalıştırıyor
+    grunt.registerTask('default', ['coffee', 'less'])
+}
