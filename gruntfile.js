27ca3f15f05d6ee7219bedbd9a4d9d1661be013b
Arman Kara <armankara@users.noreply.github.com>
2014-05-16 14:50:03 +0300

    Update gruntfile.js
    Code Refactoring

--- gruntfile.js
+++ gruntfile.js
@@ -6,30 +6,22 @@ module.exports = function(grunt) {
             compile: {
                 files: {
                     'site.js': 'coffee/site.coffee' // site.coffee'de yazdıklarımızı site.js' compile ediyoruz
-
-                }
-
-            }
-
-        },
+                     }
+                 }
+             },
         less: { //pluginlerin kendi konfigürasyon parametreleri
             compile: {
                 files: {
                     'site.css': 'less/site.less' // site.less'de yazdıklarımızı site.css' compile ediyoruz
-
-                }
-
-            }
-
-        },
-
-
-
-    });
+                     }
+                 }
+            },
+         });
+         
     // Yüklenecek grunt plugin'leri
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     //Burada da task'leri oluşturuyoruz
     //Burada default task gidip  "coffee" ve "less" taskini çalıştırıyor
     grunt.registerTask('default', ['coffee', 'less'])
-}
+  }
